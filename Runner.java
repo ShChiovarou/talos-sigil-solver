58bc7d14ffe3d9c7278e58205edf39a0d0a34a9d
Shane Chiovarou <s.chevy11@gmail.com>
2026-02-17 11:59:42 -0500

    boilerplate

---
+++ Runner.java
@@ -0,0 +1,12 @@
+import engine.Board;
+import engine.Sigil;
+import solver.Solver;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String... newyearargs) {
+        System.out.println("Starting Sigil Puzzle Solver");
+    }
+}
